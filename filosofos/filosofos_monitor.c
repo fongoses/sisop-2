24d96dfbb54281c00d255f614dc2131d40716e33
Mario Jr <mariogasparoni@gmail.com>
2013-10-06 21:16:23 -0300

    Filosofos monitor e semaforo. Completos

--- filosofos/filosofos_monitor.c
+++ filosofos/filosofos_monitor.c
@@ -21,33 +21,96 @@ o monitor eh composto pelas funcoes come() e libera()
 #include    <string.h>
 
 #define N_FILOSOFOS 5
+#define DELAY_MAX 10
 #define PENSANDO 'T'
 #define COMENDO 'E'
 #define HUNGRY 'H'
 #define ms 1000
 
+///////////////////////////////////////////////////////////////////////////////////
+//////Variaveis compartilhadas do monitor
 int FILOSOFOS[N_FILOSOFOS];
-useconds_t delay=100*ms; //100ms
+pthread_cond_t varCondicao[N_FILOSOFOS];
+int AGE[N_FILOSOFOS]; //'idade' associada ao tempo de espera no estado 'hungry'
 
-//vars mutex
-pthread_cond_t cond;
-pthread_mutex_t mutex;
+pthread_mutex_t mutexVarCondicao[N_FILOSOFOS]; //mutex para cada var de condicao
+pthread_mutex_t mutexObjeto; //mutex para acesso dos metodos do monitor
+///////////////////////////////////////////////////////////////////////////////////
 
-//verifica se ambos garfos da esquerda e direita estao disponiveis
-int garfosDisponiveis(id){
-    if ((FILOSOFOS[(id-1)%N_FILOSOFOS] != COMENDO) && (FILOSOFOS[(id+1)%N_FILOSOFOS] != COMENDO) && (FILOSOFOS[id] == HUNGRY))
-        return 1;
-    else return -1;
-    
-}
 
+///////////////////////////////////////////////////////////////////////////////////
+//Metodos auxiliares do monitor.
+//Obs.: Os metodos auxiliares nao garantem exclusao mutua. Sendo assim,
+//o metodo chamador eh quem garante sua exclusao mutua.
+///////////////////////////////////////////////////////////////////////////////////
 //lista todos os estados 
-void printaEstado(int id){
+void printaEstado(){
     int i;
     for(i=0;i<N_FILOSOFOS;i++){
-        fprintf(stdout,"%c ",FILOSOFOS[i]);
+        fprintf(stderr,"%c ",FILOSOFOS[i]);
     }    
-    fprintf(stdout,"\n");    
+    fprintf(stderr,"\n");    
+}
+
+//lista todos as idades 
+void printaAge(){
+    int i;
+    for(i=0;i<N_FILOSOFOS;i++){
+        fprintf(stderr,"%d ",AGE[i]);
+    }    
+    fprintf(stderr,"\n");    
+}
+
+//selecao de hungry's realizada pela idade/age
+int selecionaMaisVelho(int id1,int id2,int id3){
+
+    int a1,a2,a3;
+    
+    a1=AGE[id1];
+    a2=AGE[id2];
+    a3=AGE[id3];
+    
+    if(a1>=a2)
+        if(a1>=a3) return id1;
+        else return id3;
+    else 
+        if(a2>=a3) return id2;
+        else return id3;
+}
+
+
+void envelhece(){
+    int i;
+    //aumenta idade dos processos Hungry.
+    for(i=0;i<N_FILOSOFOS;i++)
+        if (FILOSOFOS[i] == HUNGRY) AGE[i]++;
+}
+
+//selecao de hungry's realizada por sorteio.
+int sorteia3(int id1, int id2, int id3){
+
+    int n1,n2,n3;
+    n1=rand()%10;
+    n2=rand()%10;
+    n3=rand()%10;
+    
+    if(n1>=n2)
+        if(n1>=n3) return id1;
+        else return id3;
+    else 
+        if(n2>=n3) return id2;
+        else return id3;
+}
+
+int sorteia2(int id1, int id2){
+    int n1,n2;
+    n1=rand()%10;
+    n2=rand()%10;
+
+    if(n1>=n2) return n1;
+    else return n2;
+ 
+
 }
 
 //informa se ha alteracao de estado, comparando o atual com o novo
@@ -55,32 +118,84 @@ int haAlteracaoEstado(int id,char novoEstado){
     if (FILOSOFOS[id] == novoEstado)  return -1;
     else return 1;
 }
+
 //...
 void alteraEstadoFilosofo(int id,char estado){
     if (haAlteracaoEstado(id,estado)==1){
-        FILOSOFOS[id]=estado;
-        printaEstado(id);
+        FILOSOFOS[id]=estado;        
+        envelhece(); //os hungry nao estao comendo, envelhecem
+        printaEstado();
+        //printaAge();
     }
 }
 
 
-//funcoes associadas a monitores 
-//id eh a id associada a thread em execucao
+
+
+//verifica se ambos garfos da esquerda e direita estao disponiveis, 
+//sinalizando que a thread pode comer, quando ha disponibilidade.
+int garfosDisponiveis(id){
+    int felizardo;
+ 
+    if ( ((FILOSOFOS[(id+N_FILOSOFOS-1)%N_FILOSOFOS] == HUNGRY) || (FILOSOFOS[(id+1)%N_FILOSOFOS] == PENSANDO)) && (FILOSOFOS[id] == HUNGRY)){ 
+        //felizardo=sorteia3((id+N_FILOSOFOS-1)%N_FILOSOFOS,id,(id+1)%N_FILOSOFOS);
+        felizardo=selecionaMaisVelho(id,(id+N_FILOSOFOS-1)%N_FILOSOFOS,(id+1)%N_FILOSOFOS);
+        if(felizardo == id) { //se o felizardo eh o processo atual, come, senao espera os mais famintos/velhos comerem primeiro
+            alteraEstadoFilosofo(felizardo,COMENDO);             
+            pthread_cond_signal(&varCondicao[id]);
+        }
+        return 1;
+    } 
+
+
+    if ( ((FILOSOFOS[(id+N_FILOSOFOS-1)%N_FILOSOFOS] == PENSANDO) || (FILOSOFOS[(id+1)%N_FILOSOFOS] == HUNGRY)) && (FILOSOFOS[id] == HUNGRY)){ 
+        //felizardo=sorteia3((id+N_FILOSOFOS-1)%N_FILOSOFOS,id,(id+1)%N_FILOSOFOS);
+        felizardo=selecionaMaisVelho(id,(id+N_FILOSOFOS-1)%N_FILOSOFOS,(id+1)%N_FILOSOFOS);
+        if(felizardo == id) { //se o felizardo eh o processo atual, come, senao espera os mais famintos/velhos comerem primeiro
+            alteraEstadoFilosofo(felizardo,COMENDO);             
+            pthread_cond_signal(&varCondicao[id]);
+        }
+        return 1;
+    } 
+ 
+    
+    if ((FILOSOFOS[(id+N_FILOSOFOS-1)%N_FILOSOFOS] != COMENDO) && (FILOSOFOS[(id+1)%N_FILOSOFOS] != COMENDO) && (FILOSOFOS[id] == HUNGRY)){
+        alteraEstadoFilosofo(id,COMENDO);
+        pthread_cond_signal(&varCondicao[id]);
+        return 1;
+    }
+    else return -1;
+    
+}
+///////////////////////////////////////////////////////////////////////////////////
+//Metodos principais do monitor.
+//Esses metodos sao os acessiveis pelo usuario do monitor.
+//Ambos garantem exclusao mutua atraves da variavel MUTEX 'mutexObjeto'
 void pegaGarfo(int id){
-    pthread_mutex_lock(&mutex);
-    alteraEstadoFilosofo(id,'H');
-    while(garfosDisponiveis(id) <0){
-        pthread_cond_wait(&cond,&mutex);    
-    }   
-    alteraEstadoFilosofo(id,'E');
-          
+    pthread_mutex_lock(&mutexObjeto);
+
+    alteraEstadoFilosofo(id,HUNGRY);
+    garfosDisponiveis(id);
+    while(FILOSOFOS[id] != COMENDO){       
+        pthread_mutex_unlock(&mutexObjeto);
+        pthread_cond_wait(&varCondicao[id],&mutexVarCondicao[id]);
+        pthread_mutex_lock(&mutexObjeto); 
+    }
+     
+    alteraEstadoFilosofo(id,COMENDO);
+    pthread_mutex_unlock(&mutexObjeto);
 }
 
 //politica: signal and continue
 void largaGarfo(int id){
+    pthread_mutex_lock(&mutexObjeto);
+
+    AGE[id] = 0;
     alteraEstadoFilosofo(id,'T');
-    pthread_cond_signal(&cond);
-    pthread_mutex_unlock(&mutex);
+    garfosDisponiveis((id+1)%N_FILOSOFOS);
+    garfosDisponiveis((id+N_FILOSOFOS-1)%N_FILOSOFOS);
+
+    pthread_mutex_unlock(&mutexObjeto); 
 }
 
 void *filosofando(void *arg){
@@ -89,26 +204,36 @@ void *filosofando(void *arg){
 
     while(1){
         pegaGarfo(id);
-        sleep(rand()%10); //pensa/come/hungry por um tempo
+        sleep(rand()%DELAY_MAX); //pensa/come/hungry por um tempo
         largaGarfo(id);
-        sleep(rand()%10);                  
+        sleep(rand()%DELAY_MAX); 
     }
 }
 
 //programa principal
-int main(int *argc, char ** argv){
-
-    pthread_cond_init(&cond,NULL);
-    pthread_mutex_init(&mutex,NULL);
+int main(int argc, char ** argv){
 
     int i;
+    
+    useconds_t delay=100*1000; //100ms
    
+    pthread_t filosofosThread[N_FILOSOFOS];
+
+    //
+    memset(AGE,0,sizeof(AGE));
+    
     //filosofos iniciam pensando 
     memset(FILOSOFOS,PENSANDO,sizeof(FILOSOFOS)); 
-    pthread_t filosofosThread[N_FILOSOFOS];
     
-    
-    for(i=0;i<N_FILOSOFOS;i++){
+    //1 mutex para controle do lock do objeto (ou seja,
+    //de todas suas variaveis)
+    pthread_mutex_init(&mutexObjeto,NULL);
+
+
+    //obs.: o mutex da variavel de condicao, um para cada var de condicao.
+    for(i=0;i<N_FILOSOFOS;i++){        
+        pthread_cond_init(&varCondicao[i],NULL);
+        pthread_mutex_init(&mutexVarCondicao[i],NULL);
         pthread_create(&filosofosThread[i],NULL,filosofando,(void*)&i);
         usleep(delay); //dorme um pouco, para n incrementar o i antes da thread inicializar
     }
--- filosofos/filosofos_semaforo.c
+++ filosofos/filosofos_semaforo.c
@@ -20,19 +20,64 @@ utilizando semaforos unnamed
 #define N_FILOSOFOS 5
 #define PENSANDO 'T'
 #define COMENDO 'E'
+#define HUNGRY 'H'
 #define ms 1000
-char FILOSOFOS[N_FILOSOFOS]; //sc
-sem_t semaforoVarFilosofos; //Semaforo para sc
+
+//////Variaveis compartilhadas do monitor
+int FILOSOFOS[N_FILOSOFOS];
+sem_t semaforoObjeto; //Semaforo para sc
+sem_t semaforoGarfos[N_FILOSOFOS]; //Semaforo para sc
+int AGE[N_FILOSOFOS]; //'idade' associada ao tempo de espera no estado 'hungry'
+///////////////////////////////////////////////////////////////////////////////////
+
+
 useconds_t delay=100*ms; //100ms
 
 //lista todos os estados 
-void printaEstado(int id){
+void printaEstado(){
     int i;
     for(i=0;i<N_FILOSOFOS;i++){
         fprintf(stdout,"%c ",FILOSOFOS[i]);
     }    
     fprintf(stdout,"\n");    
 }
+
+//lista todos as idades 
+void printaAge(){
+    int i;
+    for(i=0;i<N_FILOSOFOS;i++){
+        fprintf(stderr,"%d ",AGE[i]);
+    }    
+    fprintf(stderr,"\n");    
+}
+
+//selecao de hungry's realizada pela idade/age
+int selecionaMaisVelho(int id1,int id2,int id3){
+
+    int a1,a2,a3;
+    
+    a1=AGE[id1];
+    a2=AGE[id2];
+    a3=AGE[id3];
+    
+    if(a1>=a2)
+        if(a1>=a3) return id1;
+        else return id3;
+    else 
+        if(a2>=a3) return id2;
+        else return id3;
+}
+
+
+void envelhece(){
+    int i;
+    //aumenta idade dos processos Hungry.
+    for(i=0;i<N_FILOSOFOS;i++)
+        if (FILOSOFOS[i] == HUNGRY) AGE[i]++;
+}
+
+
+
 
 //informa se ha alteracao de estado, comparando o atual com o novo
 int haAlteracaoEstado(int id,char novoEstado){
@@ -44,48 +89,68 @@ int haAlteracaoEstado(int id,char novoEstado){
 void alteraEstadoFilosofo(int id,char estado){
     if (haAlteracaoEstado(id,estado)==1){
         FILOSOFOS[id]=estado;
-        printaEstado(id);
+        envelhece();
+        printaEstado();
+        printaAge();
     }
 }
-
-//...
-void come(id){
-    alteraEstadoFilosofo(id,'E');
-}
-
-//...
-void pensa(id){
-    alteraEstadoFilosofo(id,'T');
-}
-
-//...
-void hungry(id){
-    alteraEstadoFilosofo(id,'H');
-}
-
 //verifica se ambos garfos da esquerda e direita estao disponiveis
 int garfosDisponiveis(id){
-    if ((FILOSOFOS[(id-1)%N_FILOSOFOS] != COMENDO) && (FILOSOFOS[(id+1)%N_FILOSOFOS] != COMENDO))
+
+    int felizardo;
+
+    if ( (FILOSOFOS[(id+N_FILOSOFOS-1)%N_FILOSOFOS] == HUNGRY) && (FILOSOFOS[(id+1)%N_FILOSOFOS] == PENSANDO) && (FILOSOFOS[id] == HUNGRY)){ 
+        //felizardo=sorteia3((id+N_FILOSOFOS-1)%N_FILOSOFOS,id,(id+1)%N_FILOSOFOS);
+        felizardo=selecionaMaisVelho(id,(id+N_FILOSOFOS-1)%N_FILOSOFOS,(id+1)%N_FILOSOFOS);
+        if(felizardo == id) { //se o felizardo eh o processo atual, come, senao espera os mais famintos/velhos comerem primeiro
+            alteraEstadoFilosofo(felizardo,COMENDO);
+            sem_post(&semaforoGarfos[id]);
+        }
         return 1;
+    } 
+
+   if ( (FILOSOFOS[(id+N_FILOSOFOS-1)%N_FILOSOFOS] == PENSANDO) && (FILOSOFOS[(id+1)%N_FILOSOFOS] == HUNGRY) && (FILOSOFOS[id] == HUNGRY)){ 
+        //felizardo=sorteia3((id+N_FILOSOFOS-1)%N_FILOSOFOS,id,(id+1)%N_FILOSOFOS);
+        felizardo=selecionaMaisVelho(id,(id+N_FILOSOFOS-1)%N_FILOSOFOS,(id+1)%N_FILOSOFOS);
+        if(felizardo == id) { //se o felizardo eh o processo atual, come, senao espera os mais famintos/velhos comerem primeiro
+            alteraEstadoFilosofo(felizardo,COMENDO);
+            sem_post(&semaforoGarfos[id]);
+        }
+        return 1;
+    } 
+        
+    if ((FILOSOFOS[(id+N_FILOSOFOS-1)%N_FILOSOFOS] != COMENDO) && (FILOSOFOS[(id+1)%N_FILOSOFOS] != COMENDO) && (FILOSOFOS[id] == HUNGRY)){
+        alteraEstadoFilosofo(id,COMENDO);
+        sem_post(&semaforoGarfos[id]);
+        return 1;
+    }
     else return -1;
+
     
 }
 
 
-void tentaPegarGarfo(int id){
+void pegaGarfo(int id){
 
-    sem_wait(&semaforoVarFilosofos);
+    sem_wait(&semaforoObjeto); //adquire semaforo    
 
-    if (garfosDisponiveis(id)==1){
-        come(id);      
-        sem_post(&semaforoVarFilosofos); //libera semaforo
-        sleep(rand()%10); //come por um tempo
-        sem_wait(&semaforoVarFilosofos);
-        pensa(id);
-        
-    } else hungry(id);    
-    
-    sem_post(&semaforoVarFilosofos);
+    alteraEstadoFilosofo(id,HUNGRY);    
+    garfosDisponiveis(id);
+
+    while(FILOSOFOS[id]!=COMENDO){
+        sem_post(&semaforoObjeto);
+        sem_wait(&semaforoGarfos[id]);
+        sem_post(&semaforoObjeto);
+    }
+
+    sem_post(&semaforoObjeto); //libera semaforo
+}     
+
+void largaGarfo(int id){
+    sem_wait(&semaforoObjeto);    
+    AGE[id]=0;
+    alteraEstadoFilosofo(id,PENSANDO);
+    sem_post(&semaforoObjeto);
 }
 
 void *filosofando(void *arg){
@@ -93,21 +158,26 @@ void *filosofando(void *arg){
     int id = *(int*)arg;
 
     while(1){
-        sleep(rand()%10); //pensa/come/hungry por um tempo
-        tentaPegarGarfo(id);          
+        pegaGarfo(id);          
+        sleep(rand()%10); //come por um tempo
+        largaGarfo(id);
     }
 }
 
 int main(int argc, char *argv[ ]) {
    
     int i;
+
+    
+    memset(AGE,0,sizeof(AGE));
     //filosofos iniciam pensando 
     memset(FILOSOFOS,PENSANDO,sizeof(FILOSOFOS)); 
     pthread_t filosofosThread[N_FILOSOFOS];
     
-    sem_init(&semaforoVarFilosofos,0,1); //exclusao mutua (S=1) na variavel compartilhada 
+    sem_init(&semaforoObjeto,0,1); //exclusao mutua (S=1) na variavel compartilhada 
     
     for(i=0;i<N_FILOSOFOS;i++){
+        sem_init(&semaforoGarfos[i],0,1);
         pthread_create(&filosofosThread[i],NULL,filosofando,(void*)&i);
         usleep(delay); //dorme um pouco, para n incrementar o i antes da thread inicializar
     }
